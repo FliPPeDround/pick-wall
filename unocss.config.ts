cee777f5a15069b356cea44bcb8046e775b79d8b
jojoy <734243792@qq.com>
2022-04-19 16:05:16 +0800

    更新文件 unocss.config.ts

--- unocss.config.ts
+++ unocss.config.ts
@@ -3,7 +3,6 @@ import {
   presetAttributify,
   presetIcons,
   presetUno,
-  presetWebFonts,
   // transformerDirectives,
   // transformerVariantGroup,
 } from 'unocss'
@@ -19,14 +18,7 @@ export default defineConfig({
     presetIcons({
       scale: 1.2,
       warn: true,
-    }),
-    presetWebFonts({
-      fonts: {
-        sans: 'DM Sans',
-        serif: 'DM Serif Display',
-        mono: 'DM Mono',
-      },
-    }),
+    })
   ],
   // transformers: [
   //   transformerDirectives(),
