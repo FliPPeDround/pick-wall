1a706a3594b6ad57c7ea8baea016f43e9aa84f44
jojoypeng <jojoypeng@tencent.com>
2022-04-20 15:58:12 +0800

    feat: 拖动获取fill

--- src/composables/init.ts
+++ src/composables/init.ts
@@ -1,6 +1,6 @@
 import { PickWallInit } from './logic'
 
-const init = new PickWallInit(document.body.offsetWidth, document.body.offsetHeight, 30)
+const init = new PickWallInit(document.body.offsetWidth, document.body.offsetHeight, 100)
 export const config = computed(() => init.config.value)
 
 export default init
--- src/composables/logic.ts
+++ src/composables/logic.ts
@@ -52,7 +52,7 @@ export class PickWallInit {
             x: -Math.ceil(newX / self.rectLen),
             y: -Math.ceil(newY / self.rectLen),
           }
-          self.throttleFn(move)
+          self.dragRect(move)
           return {
             x: newX,
             y: newY,
@@ -80,8 +80,8 @@ export class PickWallInit {
     const res = await getConfigRects(state)
 
     res.forEach((block) => {
-      if (this.config.value.configRects?.[block.y]?.[block.x])
-        this.config.value.configRects[block.y][block.x].fill = block.fill
+      if (this.config.value.configRects?.[block.y - state.startY]?.[block.x - state.startX])
+        this.config.value.configRects[block.y - state.startY][block.x - state.startX].fill = block.fill
     })
   }
 
@@ -113,85 +113,111 @@ export class PickWallInit {
     this.color.value = color
   }
 
-  throttleFn = useThrottleFn(async(move: Point) => {
+  dragRect = useThrottleFn(async(move: Point) => {
     const newrow = move.x - this.move.x
     const newcol = move.y - this.move.y
     this.move = move
 
-    if (newcol < 0) {
-      const configRectsLast = this.config.value.configRects[0][0]
-      const newRectsY = Array.from({ length: -newcol }, (_, y) =>
-        Array.from({ length: this.config.value.configRects[0].length },
-          (_, x) => ({
-            x: this.config.value.configRects[this.config.value.configRects.length - 1][x].x,
-            y: configRectsLast.y + (newcol + y) * this.rectLen,
-            width: this.rectLen,
-            height: this.rectLen,
-            fill: '#FFF',
-            stroke: '#9B9B9B82',
-            strokeWidth: 1,
-          }),
-        ),
-      )
-      this.config.value.configRects.unshift(...newRectsY)
-      this.config.value.configRects.splice(newcol)
-    }
+    if (newcol < 0)
+      this.unshiftRectListCol(newcol)
 
-    if (newcol > 0) {
-      const configRectsLast = this.config.value.configRects[this.config.value.configRects.length - 1][0]
-      const newRectsY = Array.from({ length: newcol }, (_, y) =>
-        Array.from({ length: this.config.value.configRects[0].length },
-          (_, x) => ({
-            x: this.config.value.configRects[this.config.value.configRects.length - 1][x].x,
-            y: configRectsLast.y + (y + 1) * this.rectLen,
-            width: this.rectLen,
-            height: this.rectLen,
-            fill: '#FFF',
-            stroke: '#9B9B9B82',
-            strokeWidth: 1,
-          }),
-        ),
-      )
-      // await this.getRects({
-      //   startX: newRectsY[0][0].x / 30,
-      //   startY: newRectsY[0][0].y / 30,
-      //   endX: newRectsY[newRectsY.length - 1][newRectsY[newRectsY.length - 1].length - 1].x / 30,
-      //   endY: newRectsY[newRectsY.length - 1][newRectsY[newRectsY.length - 1].length - 1].y / 30,
-      // })
+    if (newcol > 0)
+      this.pushRectListCol(newcol)
 
-      this.config.value.configRects.push(...newRectsY)
-      this.config.value.configRects.splice(0, newcol)
-    }
-    if (newrow > 0) {
-      this.config.value.configRects.forEach((row) => {
-        row.splice(0, newrow)
-        const rowLen = row.length
-        row.push(...Array.from({ length: newrow }, (_, x) => ({
-          x: row[rowLen - 1].x + this.rectLen * (x + 1),
-          y: row[rowLen - 1].y,
-          width: this.rectLen,
-          height: this.rectLen,
-          fill: '#FFF',
-          stroke: '#9B9B9B82',
-          strokeWidth: 1,
-        })))
-      })
-    }
-    if (newrow < 0) {
-      this.config.value.configRects.forEach((row) => {
-        row.splice(newrow)
-        const newRowList = Array.from({ length: -newrow }, (_, x) => ({
-          x: row[0].x + (this.rectLen * (newrow + x)),
-          y: row[0].y,
-          width: this.rectLen,
-          height: this.rectLen,
-          fill: '#FFF',
-          stroke: '#9B9B9B82',
-          strokeWidth: 1,
-        }))
+    if (newrow > 0)
+      this.pushRectListRow(newrow)
 
-        row.unshift(...newRowList)
-      })
-    }
+    if (newrow < 0)
+      this.unshiftRectListRow(newrow)
+
+    const state = this.getRectsState(this.config.value.configRects)
+    await this.getRects(state)
   }, 500)
+
+  async unshiftRectListCol(newcol: number) {
+    const configRectsLast = this.config.value.configRects[0][0]
+    const newColList = Array.from({ length: -newcol }, (_, y) => Array.from({ length: this.config.value.configRects[0].length },
+      (_, x) => ({
+        x: this.config.value.configRects[this.config.value.configRects.length - 1][x].x,
+        y: configRectsLast.y + (newcol + y) * this.rectLen,
+        width: this.rectLen,
+        height: this.rectLen,
+        fill: '#FFF',
+        stroke: '#9B9B9B82',
+        strokeWidth: 1,
+      }),
+    ),
+    )
+    this.config.value.configRects.unshift(...newColList)
+    this.config.value.configRects.splice(newcol)
+  }
+
+  async pushRectListCol(newcol: number) {
+    const configRectsLast = this.config.value.configRects[this.config.value.configRects.length - 1][0]
+    const newColList = Array.from({ length: newcol }, (_, y) => Array.from({ length: this.config.value.configRects[0].length },
+      (_, x) => ({
+        x: this.config.value.configRects[this.config.value.configRects.length - 1][x].x,
+        y: configRectsLast.y + (y + 1) * this.rectLen,
+        width: this.rectLen,
+        height: this.rectLen,
+        fill: '#FFF',
+        stroke: '#9B9B9B82',
+        strokeWidth: 1,
+      }),
+    ),
+    )
+
+    this.config.value.configRects.push(...newColList)
+    this.config.value.configRects.splice(0, newcol)
+  }
+
+  async pushRectListRow(newrow: number) {
+    for (let i = 0; i < this.config.value.configRects.length; i++) {
+      const row = this.config.value.configRects[i]
+      row.splice(0, newrow)
+      const rowLen = row.length
+      const newRowList = Array.from({ length: newrow }, (_, x) => ({
+        x: row[rowLen - 1].x + this.rectLen * (x + 1),
+        y: row[rowLen - 1].y,
+        width: this.rectLen,
+        height: this.rectLen,
+        fill: '#FFF',
+        stroke: '#9B9B9B82',
+        strokeWidth: 1,
+      }))
+
+      row.push(...newRowList)
+    }
+  }
+
+  async unshiftRectListRow(newrow: number) {
+    for (let i = 0; i < this.config.value.configRects.length; i++) {
+      const row = this.config.value.configRects[i]
+      row.splice(newrow)
+      const newRowList = Array.from({ length: -newrow }, (_, x) => ({
+        x: row[0].x + (this.rectLen * (newrow + x)),
+        y: row[0].y,
+        width: this.rectLen,
+        height: this.rectLen,
+        fill: '#FFF',
+        stroke: '#9B9B9B82',
+        strokeWidth: 1,
+      }))
+
+      row.unshift(...newRowList)
+    }
+  }
+
+  getRectsState(rectList: BlockState[][]) {
+    const startX = rectList[0][0].x / this.rectLen
+    const startY = rectList[0][0].y / this.rectLen
+    const endX = (rectList as BlockState[][])[rectList.length - 1][(rectList as BlockState[][])[rectList.length - 1].length - 1].x / this.rectLen
+    const endY = (rectList as BlockState[][])[rectList.length - 1][(rectList as BlockState[][])[rectList.length - 1].length - 1].y / this.rectLen
+    return {
+      startX,
+      startY,
+      endX,
+      endY,
+    }
+  }
 }
